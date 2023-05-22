5ec92c8db0b6ea06552fb89db7b7d40df4954537
Katerina Sipatova <katerina.utg@gmail.com>
2023-05-22 13:29:57 +0400

    ImportError problem is solved

--- conftest.py
+++ conftest.py
@@ -6,8 +6,6 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    # parser.addoption('--browser_name', action='store', default='chrome',
-    #                  help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default='en',
                      help="Choose language: ru, en, es, ... (default - en)")
 
@@ -21,23 +19,3 @@ def browser(request):
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
-
-
-
-
-# def browser(request):
-#     browser_name = request.config.getoption("browser_name")
-#     user_language = request.config.getoption("language")
-#     browser = None
-#     if browser_name == "chrome":
-#         options = Options()
-#         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-#         browser = webdriver.Chrome(options=options)
-#     elif browser_name =="firefox":
-#         fp = webdriver.FirefoxProfile()
-#         fp.set_preference("intl.accept_languages", user_language)
-#         browser = webdriver.Firefox(firefox_profile=fp)
-#     else:
-#         raise pytest.UsageError("--browser_name should be chrome or firefox")
-#     yield browser
-#     browser.quit()
